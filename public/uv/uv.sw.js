f4f233db75034bcf19c0407ab66e013d1f7aaf6f
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-03-27 16:28:56 -0700

    Update uv.sw.js

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,9 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.0.2/dist/uv.bundle.js');
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.0.2/dist/uv.config.js');
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.0.2/dist/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(sw.fetch(event));
-});
