d4716bf4ea11576004331106e97a24727d96c08f
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-03-27 16:44:55 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.student-portal.cloud/bare/', // Public bare for testing
+    bare: 'https://uv.student-portal.cloud/bare/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.0.2/dist/uv.handler.js',
