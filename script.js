ba63b2f3248ba83a248e87ed325d352995f78a40
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-03-27 16:16:24 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('go-btn').addEventListener('click', () => {
-    const url = document.getElementById('url-input').value;
-    if (url) {
-        // Redirects to our proxy endpoint with the URL as a query
-        window.location.href = `/proxy?url=${encodeURIComponent(url)}`;
-    }
-});
